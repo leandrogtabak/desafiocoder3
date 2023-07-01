5085c4182c54995c9be6961cc6c3d044e0ebb67b
Leandro Tabak <leandrogtabak@gmail.com>
2023-07-01 14:54:17 +0300

    Agregando varible de ent

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const { Contenedor } = require('./handleFiles.js');
+const express = require('express')
+require('dotenv').config()
+const { Contenedor } = require('./handleFiles.js')
 
-const app = express();
+const app = express()
 
-const PORT = 8081;
 
 const producto1 = {
   title: 'Pantalon',
   price: '$1500',
   thumbnail: 'http://random1.com',
-};
+}
 const producto2 = {
   title: 'Sombrero',
   price: '$950',
   thumbnail: 'http://random2.com',
-};
+}
 const producto3 = {
   title: 'Corbata',
   price: '$875',
   thumbnail: 'http://random3.com',
-};
+}
 
-const myContainer = new Contenedor('./productos.txt');
+const myContainer = new Contenedor('./productos.txt')
 
 // async function salvarProductos() {
 //   //show save products
@@ -32,31 +32,33 @@ const myContainer = new Contenedor('./productos.txt');
 
 // salvarProductos();
 
-const server = app.listen(PORT, () => {
-  console.log(`Servidor http corriendo en el puerto ${server.address().port}`);
-});
+const server = app.listen(process.env.PORT || 4000, () => {
+  console.log(`Servidor http corriendo en el puerto ${process.env.PORT || 4000}`)
+})
 
 app.get('/', (req, res) => {
-  res.send('Verificar productos ingresando a /productos');
-});
+  res.send('Verificar productos ingresando a /productos')
+})
 
 app.get('/productos', async (req, res) => {
   try {
-    let productos = await myContainer.getAll();
-    res.send(productos);
+    let productos = await myContainer.getAll()
+    res.send(productos)
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
-});
+})
 
 app.get('/productoRandom', async (req, res) => {
   try {
-    let productos = await myContainer.getAll();
-    const idRandom = Math.floor(Math.random() * productos.length);
-    res.send(productos[idRandom]);
+    let productos = await myContainer.getAll()
+    const idRandom = Math.floor(Math.random() * productos.length)
+    res.send(productos[idRandom])
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
-});
+})
 
-server.on('error', (error) => console.log(`Error en el servidor: ${error}`));
+server.on('error', (error) => {
+  console.log(`Error en el servidor: ${error}`)
+})
